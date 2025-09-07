7d349ff401f5fd54124b5c0644616dbe95673425
Ashar-Ahmed11 <ashar.a.hmd11@gmail.com>
2025-09-07 16:41:08 +0500

    App Engine Deployment

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const URI = 'mongodb://localhost:27017/foodDelivery'
+const URI = 'mongodb+srv://asharahmd11_db_user:e9AKme0rHxW4AIP5@cluster0.nxztxig.mongodb.net/foodDelivery'
 
 mongoose.set("strictQuery", false);
 const connectToMongo = () => mongoose.connect(URI, () => {
